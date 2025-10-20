f3ab06dd686abcd409ddcb083b2e33bc9d95ea08
cxrvus <benito.m.rabe@gmail.com>
2025-10-20 16:11:27 +0200

    add Moore's Law calculation

--- crypto/ek1_ws25_ex1.js
+++ crypto/ek1_ws25_ex1.js
@@ -1,7 +1,7 @@
 // pre-defined
 
 let K = 2 ** 128; 				// possible keys
-let ASIC_speed = 7 * (10 ** 8); 	// keys per second
+let ASIC_speed = 7 * (10 ** 8); // keys per second
 let budget = 1000000;			// euros
 let ASIC_price = 80; 			// 40 * 2, due to overhead
 let universe_age = 10 ** 10;	// years
@@ -9,9 +9,30 @@ let universe_age = 10 ** 10;	// years
 // obtained
 
 let ASIC_count = budget / ASIC_price;								// ASICs
-let search_duration_sec = K / (ASIC_count * ASIC_speed);			// seconds
-let search_duration = search_duration_sec / (60 * 60 * 24 * 365);	// years
+
+function search_duration_sec(speed) {
+	return K / (ASIC_count * speed);
+}
+
+let search_duration = search_duration_sec(ASIC_speed) / (60 * 60 * 24 * 365);	// years
 
 let relative_duration = search_duration / universe_age;
 
 console.log(`- ${ASIC_count} ASICs\n- ${search_duration} years (${relative_duration} times the age of the universe)`);
+
+let months_passed = 0;
+
+// Moore's Law
+for (let i = 0; i < 100; i++) {
+	let secs = search_duration_sec(ASIC_speed);
+	let hrs = secs / (60 * 60);
+
+	if (hrs <= 24) {
+		break;
+	}
+
+	months_passed += 18;
+	ASIC_speed *= 2;
+}
+
+console.log(`- solvable in <24h after ${months_passed / 12} years`);
