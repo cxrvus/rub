77e0b3474a2edf891230a396a85c1a083c87e484
cxrvus <benito.m.rabe@gmail.com>
2025-10-25 03:29:25 +0200

    create ek1_ws25_hw1.js

---
+++ crypto/ek1_ws25_hw1.js
@@ -0,0 +1,30 @@
+const gpu_speed = 16.2 * 10**7;
+const fpga_speed = 10.5 * 10**8;
+
+const key_sizes = [56, 128, 256];
+
+const get_all_tries = size => 2 ** size;
+const get_avg_tries = size => 2 ** (size-1);
+
+const all_tries = key_sizes.map(size => get_all_tries(size));
+const avg_tries = key_sizes.map(size => get_avg_tries(size));
+
+const secs_to_years = secs => secs / (60 * 60 * 24 * 365);
+const get_search_dur = (speed, tries) => secs_to_years(tries / speed);
+const print_search_dur = (target, speed, tries, bits) => {
+	console.log(`${target} | ${bits} bits | ${get_search_dur(speed, tries)}`);
+}
+
+const test_hardware = try_counts => 
+	try_counts.forEach((try_count, i) => {
+		print_search_dur('GPU ', gpu_speed, try_count, key_sizes[i]);
+		print_search_dur('FPGA', fpga_speed, try_count, key_sizes[i]);
+	})
+;
+
+console.log('Worst Case:');
+test_hardware(all_tries)
+
+console.log();
+console.log('Average Case:');
+test_hardware(avg_tries);
