a2931a569d26decd05786a205a64d9adbf6e54e6
cxrvus <benito.m.rabe@gmail.com>
2026-01-15 17:11:55 +0100

    create hamming_distance.s

---
+++ comp_arch/hamming_distance.s
@@ -0,0 +1,31 @@
+# loop 32 times (for a 32-bit word size)
+# use t4 as a count-down variable
+li t4 32
+
+# we can re-use our hamming-weight code,
+# since HD=HW(XOR(a,b))
+xor t0 t1 t0
+li t1 0
+
+# calculate hamming-weight of t0
+loop:
+    beqz t4 end
+    
+    # check LSB:
+    # t3 += t2 & 1
+    andi t2 t0 1
+    add t3 t3 t2
+    
+    # shift next bit to LSB
+    # t0 >>= 1
+    srli t0 t0 1
+    
+    # decrement count-down and loop
+    # t4--
+    addi t4 t4 -1
+    j loop
+
+# move result t3 into t0
+# t0 = t3
+end:
+    mv t0 t3
